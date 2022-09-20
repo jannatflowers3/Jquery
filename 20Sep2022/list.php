9fd4d8de1627dd658830a6908b1b204639348817
jannatflowers3 <jannatflowersweb@gmail.com>
2022-09-20 18:59:35 +0600

    20sep

---
+++ 20Sep2022/list.php
@@ -0,0 +1,26 @@
+<?php
+// echo $_GET['mydata_id'];
+
+
+if($_GET['mydata']=='Good'){
+    $person = array("sultana","aklima","jannat");
+    echo getdata($person);
+}
+
+else if($_GET['mydata']=='Bad'){
+    $person = array("smima","anika","bipasha");
+    echo getdata($person);
+}
+
+
+function getdata($items){
+    $output = "<ul>";
+    for($i = 0;$i<count($items);$i++){
+        $output .="<li>".$items[$i]."</li>";
+    }
+    $output.= "</ul>";
+    return $output;
+}
+// echo getdata($person);
+
+?>
